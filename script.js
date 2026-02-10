b215f626f764ca28a2d51826e6d91cc6074baff3
bilal <babubilal712@gmail.com>
2026-02-10 17:29:29 +0530

    Updated hamburger menu and responsive changes

---
+++ script.js
@@ -0,0 +1,28 @@
+const text = "MERN Stack Developer";
+let i = 0;
+
+function typingEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typingEffect, 100);
+  }
+}
+
+typingEffect();
+
+img.addEventListener("mouseenter", () => {
+  speed = 0.01;
+});
+
+img.addEventListener("mouseleave", () => {
+  speed = 0.05;
+});
+// Hamburger toggle
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("header nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
